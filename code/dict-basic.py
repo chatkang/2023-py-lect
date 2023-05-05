f754221d6815a6b9b26d819a55a9f97411b344ee
chatkang <126311686+chatkang@users.noreply.github.com>
2023-05-05 09:42:19 +0000

    add

--- code/dict-basic.py
+++ code/dict-basic.py
@@ -1,10 +1,13 @@
 # %%
 help(dict)
+
 # %%
 a = {'python': 1990, 'c': 1972}
 a
+
 # %%
 type(a)
+
 # %%
 b = dict((('python', 1990), ('c', 1972)))
 b
@@ -16,12 +19,15 @@ c
 # %%
 a['python'] = 1989
 a
+
 # %%
 a['c#'] = 2000
 a
+
 # %%
 for i in a:
     print(i)
+
 # %%
 for k in a:
     print(k, a[k])
@@ -29,25 +35,81 @@ for k in a:
 # %%
 num = dict(one=1, two=2)
 num
+
 # %%
 num['three'] = 3
 num
+
 # %%
 for k in num:
     print(k, '->', num[k])
+
 # %%
 num2 = dict() # {}
 type(num2)
+
 # %%
 for k in num:
     num2[num[k]] = k
 num2
+
 # %%
-num
+import math
+math.pi
+
 # %%
-e = dict([('python', 1990)])
-e
+math.e
+
 # %%
-e = dict([(list('python'), 1990)])
+help(round)
+
+# %%
+round(math.e, 2)
+
+# %%
+round(math.pi, 3)
+
+# %%
+p = dict(apple=0.4, orange=0.35, banana=0.25)
+p
+
+# %%
+for k, v in p.items():
+    p[k] = round(v * 0.9, 2)
+p
+# %%
+for i in p.items():
+    p[i[0]] = round(i[1] * 0.9, 2)
+p
+
+# %%
+lst = [1, 2, 3]
+a, b, c = lst
+c
+
+# %%
+data = lst
+print(data[0], data[1], data[2])
+
+# %%
+p
+# %%
+p2 = {}
+for k, v in p.items():
+    p2[v] = k
+p2
+
+# %%
+p
+# %%
+p3 = {}
+for k, v in p.items():
+    if v <= 0.3:
+        p3[v] = k
+p3
+
+# %%
+c = { i: i**i for i in range(5)}
+c
 
 # %%
